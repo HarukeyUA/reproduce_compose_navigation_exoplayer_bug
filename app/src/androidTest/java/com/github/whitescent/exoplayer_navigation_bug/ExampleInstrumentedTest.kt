0b43a9309a45909f6adaab710b28382edbf50dc5
whitescent <Nthily@outlook.com>
2023-09-01 20:19:04 +0800

    initial commit

---
+++ app/src/androidTest/java/com/github/whitescent/exoplayer_navigation_bug/ExampleInstrumentedTest.kt
@@ -0,0 +1,22 @@
+package com.github.whitescent.exoplayer_navigation_bug
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+  @Test
+  fun useAppContext() {
+    // Context of the app under test.
+    val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+    assertEquals("com.github.whitescent.exoplayer_navigation_bug", appContext.packageName)
+  }
+}
---
+++ app/src/main/java/com/github/whitescent/exoplayer_navigation_bug/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.github.whitescent.exoplayer_navigation_bug
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.media3.common.MediaItem
+import androidx.media3.common.Player
+import androidx.media3.ui.AspectRatioFrameLayout
+import androidx.media3.ui.PlayerView
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.github.whitescent.exoplayer_navigation_bug.player.ExoPlayerLifecycleEvents
+import com.github.whitescent.exoplayer_navigation_bug.player.rememberExoPlayerInstance
+import com.github.whitescent.exoplayer_navigation_bug.ui.theme.Exoplayer_navigation_bugTheme
+
+const val url = "https://storage.googleapis.com/exoplayer-test-media-0/play.mp3"
+
+@SuppressLint("UnsafeOptInUsageError")
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Exoplayer_navigation_bugTheme {
+                val navController = rememberNavController()
+                NavHost(navController, startDestination = "home") {
+                    composable("home") {
+                        Box(Modifier.fillMaxSize().background(Color.Red), Alignment.Center) {
+                            Column {
+                                Text("HOME")
+                                Button(onClick = { navController.navigate("video") }) {
+                                }
+                            }
+                        }
+                    }
+                    composable("video") {
+                        val exoPlayer = rememberExoPlayerInstance()
+                        ExoPlayerLifecycleEvents(exoPlayer)
+                        Box(Modifier.fillMaxSize(), Alignment.Center) {
+                            AndroidView(
+                                factory = { context ->
+                                    PlayerView(context).apply {
+                                        player = exoPlayer
+                                        setShowBuffering(PlayerView.SHOW_BUFFERING_ALWAYS)
+                                        resizeMode = AspectRatioFrameLayout.RESIZE_MODE_FIT
+                                        useController = false
+                                    }
+                                },
+                                onReset = {
+                                    it.player = exoPlayer
+                                },
+                            )
+                            LaunchedEffect(Unit) {
+                                exoPlayer.run {
+                                    setMediaItem(
+                                        MediaItem.Builder().setMediaId(url).setUri(url).build(),
+                                    )
+                                    prepare()
+                                    repeatMode = Player.REPEAT_MODE_ONE
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
